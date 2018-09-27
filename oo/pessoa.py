93873476b2ea20ec6c28f98632bbc1a90053e7b4
Cécil Malone Souza Cardozo <cecilcardozo@gmail.com>
2018-09-27 11:59:32 -0300

    Inserido atributo complexo na instância da classe.

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,17 +1,20 @@
 class Pessoa:
-    def __init__(self, nome=None, idade=35):
+    def __init__(self, *filhos, nome=None, idade=35):
         self.idade = idade
         self.nome = nome
+        self.filhos = list(filhos)
 
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
+
 if __name__ == '__main__':
-    p = Pessoa('Malone')
-    print(Pessoa.cumprimentar(p))
-    print(id(p))
-    print(p.cumprimentar())
-    print(p.nome)
-    p.nome = 'Cécil'
-    print(p.nome)
-    print(p.idade)
+    cecil = Pessoa(nome='Cécil')
+    malone = Pessoa(cecil, nome='Malone')
+    print(Pessoa.cumprimentar(malone))
+    print(id(malone))
+    print(malone.cumprimentar())
+    print(malone.nome)
+    print(malone.idade)
+    for filho in malone.filhos:
+        print(filho.nome)
