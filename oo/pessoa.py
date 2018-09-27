6cf8eec8c683e1a6bbe05fdd362841875828f2a8
Cécil Malone Souza Cardozo <cecilcardozo@gmail.com>
2018-09-27 12:19:06 -0300

    Criação do atributo "olhos" para a classe.

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,4 +1,6 @@
 class Pessoa:
+    olhos = 2
+
     def __init__(self, *filhos, nome=None, idade=35):
         self.idade = idade
         self.nome = nome
@@ -20,5 +22,12 @@ if __name__ == '__main__':
         print(filho.nome)
     malone.sobrenome='Souza'
     del malone.filhos
+    malone.olhos = 1
+    del malone.olhos
     print(malone.__dict__)
     print(cecil.__dict__)
+    Pessoa.olhos = 3
+    print(Pessoa.olhos)
+    print(malone.olhos)
+    print(cecil.olhos)
+    print(id(Pessoa.olhos), id(malone.olhos), id(cecil.olhos))
