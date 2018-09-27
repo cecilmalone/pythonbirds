11b879fba631204a1c23962585bbc5335c28bfe9
Cécil Malone Souza Cardozo <cecilcardozo@gmail.com>
2018-09-27 12:35:07 -0300

    Criação de métodos de classe.

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -9,6 +9,14 @@ class Pessoa:
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
+    @staticmethod
+    def metodo_estatico():
+        return 42
+
+    @classmethod
+    def nome_e_atributos_de_class(cls):
+        return f'{cls} - olhos {cls.olhos}'
+
 
 if __name__ == '__main__':
     cecil = Pessoa(nome='Cécil')
@@ -31,3 +39,5 @@ if __name__ == '__main__':
     print(malone.olhos)
     print(cecil.olhos)
     print(id(Pessoa.olhos), id(malone.olhos), id(cecil.olhos))
+    print(Pessoa.metodo_estatico(), malone.metodo_estatico())
+    print(Pessoa.nome_e_atributos_de_class(), malone.nome_e_atributos_de_class())
